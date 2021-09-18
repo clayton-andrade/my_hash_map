94c2e6774bfaef9bf5dc3f66089205186266dba9
clayton-andrade <clayton.andrade2009@gmail.com>
2021-09-18 12:02:12 -0300

    Final commit

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ struct MyMap<K, V> {
     buckets: Vec<Option<(K, V)>>,
 }
 
-impl<K, V> MyMap<K, V> {
+impl<K: Hash, V: Copy> MyMap<K, V> {
     fn new() -> Self {
         let mut buckets: Vec<Option<(K, V)>> = Vec::with_capacity(INITIAL_BUCKET_SIZE);
         for _ in 0..INITIAL_BUCKET_SIZE {
@@ -17,61 +17,47 @@ impl<K, V> MyMap<K, V> {
         MyMap { buckets }
     }
 
-    fn insert(&mut self, key: K, value: V)
-    where
-        K: Hash,
-    {
-        let mut hasher = DefaultHasher::new();
-        key.hash(&mut hasher);
-        let index = hasher.finish() % self.buckets.len() as u64;
-        self.buckets[index as usize] = Some((key, value));
-        // match self.buckets.iter().enumerate().find(|(_, (k, _))| *k == key).map(|(i, (_, _))| i) {
-        //     Some(i) => {
-        //         let (_, v) = self.buckets.get_mut(i).unwrap();
-        //         *v = value;
-        //     },
-        //     None => self.buckets.push((key, value)),
-        // }
+    fn insert(&mut self, key: K, value: V) {
+        let index = self.hash(&key);
+        self.buckets[index] = Some((key, value));
     }
 
-    // fn get(&self, key: &K) -> Option<&V>
-    // where
-    //     K: std::cmp::Eq,
-    // {
-    //     self.buckets.iter().find(|(k, _)| k == key).map(|(_, v)| v)
-    // }
+    fn get(&self, key: &K) -> Option<&V> {
+        let index = self.hash(key);
+        self.buckets.get(index).and_then(|elem| elem.as_ref()).map(|(_, v)| v)
+    }
 
-    // fn remove(&mut self, key: K) -> Option<V>
-    // where
-    //     K: std::cmp::Eq,
-    //     V: Copy + Clone,
-    // {
-    //     let value;
-    //     let index;
-    //     if let Some(i) = self.buckets.iter().enumerate().find(|(_, (k, _))| *k == key).map(|(i, (_, _))| i) {
-    //         let (_, v) = self.buckets.get(i).unwrap();
-    //         value = v.to_owned();
-    //         index = i;
-    //     } else {
-    //         return None;
-    //     }
-    //     self.buckets.remove(index);
-    //     Some(value)
-    // }
+    fn remove(&mut self, key: K) -> Option<V> {
+        let index = self.hash(&key);
+        if let Some(v) = self.buckets.get(index).and_then(|elem| elem.as_ref()).map(|(_, v)| *v) {
+              self.buckets.remove(index);
+              Some(v)
+        } else {
+            return None;
+        }
+    }
+
+    fn hash(&self, key: &K) -> usize {
+        let mut hasher = DefaultHasher::new();
+        key.hash(&mut hasher);
+        (hasher.finish() % self.buckets.len() as u64) as usize
+    }
 }
 
 fn main() {
     let mut map = MyMap::new();
     map.insert("maçã", 45);
-    println!("{:?}", map);
     map.insert("maçã", 56);
-    println!("{:?}", map);
     map.insert("pera", 22);
-    // assert_eq!(map.get(&"limão"), None);
-    // assert_eq!(map.get(&"pera"), Some(&22));
-    // println!("{:?}", map);
-    // assert_eq!(map.remove("maçã"), Some(56));
-    // println!("{:?}", map);
-    // assert_eq!(map.remove("limão"), None);
-    println!("{:?}", map);
+    assert_eq!(map.get(&"limão"), None);
+    assert_eq!(map.get(&"pera"), Some(&22));
+    assert_eq!(map.remove("maçã"), Some(56));
+    assert_eq!(map.remove("limão"), None);
+    for pair in &map.buckets {
+        if pair.is_none() {
+            continue;
+        }
+        println!("{}: {}", pair.unwrap().0, pair.unwrap().1);
+    }
+    
 }
